6337ad67d80ff3f54f55965172e5596acaf76b48
Uladzislava Santalava <santalova.lada25@gmail.com>
2024-04-04 18:05:41 +0200

    feat: solve problem

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -51,8 +51,10 @@ function getFunctionBody(func) {
  *  ]) => [0, 1, 2]
  *
  */
-function getArgumentsCount(/* funcs */) {
-  throw new Error('Not implemented');
+function getArgumentsCount(funcs) {
+  return funcs.map((func) => {
+    return func.length;
+  });
 }
 
 /**
