e1f57dabc192005a1b1fe8de4b70f90636648aa6
Uladzislava Santalava <santalova.lada25@gmail.com>
2024-04-04 20:42:41 +0200

    feat: solve retry problem

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -142,8 +142,19 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return () => {
+    for (let i = 0; i < attempts; i += 1) {
+      try {
+        return func();
+      } catch (error) {
+        if (i === attempts - 1) {
+          throw error;
+        }
+      }
+    }
+    return null;
+  };
 }
 
 /**
