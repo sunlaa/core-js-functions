530ea7001b40a106a93593ccce8d81a302dfc38c
Uladzislava Santalava <santalova.lada25@gmail.com>
2024-04-04 18:43:51 +0200

    feat: solve memoize problem

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -114,8 +114,17 @@ function getPolynom(...args) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const cache = new Map();
+  return (...args) => {
+    const key = JSON.stringify(args);
+    if (cache.has(key)) {
+      return cache.get(key);
+    }
+    const result = func(...args);
+    cache.set(key, result);
+    return result;
+  };
 }
 
 /**
